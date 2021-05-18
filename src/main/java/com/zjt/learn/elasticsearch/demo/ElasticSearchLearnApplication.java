1993020a88b0a47a4cf60f20a24f97a9ae6ed171
zhaojiatao <2322795718@qq.com>
2021-05-18 22:54:42 +0800

    Initial commit

---
+++ src/main/java/com/zjt/learn/elasticsearch/demo/ElasticSearchLearnApplication.java
@@ -0,0 +1,13 @@
+package com.zjt.learn.elasticsearch.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearchLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticSearchLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zjt/learn/elasticsearch/demo/ElasticSearchLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjt.learn.elasticsearch.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
