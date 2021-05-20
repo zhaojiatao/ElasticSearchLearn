b823689cc88fd3fe6af0837f35dc6a7e29c5001a
zhaojiatao <2322795718@qq.com>
2021-05-20 23:05:05 +0800

    增加match查询

--- src/main/java/com/zjt/learn/elasticsearch/demo/QueryDemo.java
+++ src/main/java/com/zjt/learn/elasticsearch/demo/QueryDemo.java
@@ -5,6 +5,7 @@ import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.Operator;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
@@ -85,6 +86,105 @@ public class QueryDemo {
         }
 
     }
+
+    @Test
+    public void matchAllQuery() throws IOException{
+        //1、创建Request
+        SearchRequest request=new SearchRequest(index);
+        request.types(type);
+
+        //2、指定查询条件
+        SearchSourceBuilder builder=new SearchSourceBuilder();
+        builder.query(QueryBuilders.matchAllQuery());
+        //ES默认只查询10条数据，如果想查询更多，添加size
+        builder.size(20);
+        request.source(builder);
+
+        //3、执行查询
+        SearchResponse resp=client.search(request,RequestOptions.DEFAULT);
+
+        //4、获取到_source的数据，并展示
+        for(SearchHit hit:resp.getHits().getHits()){
+            Map<String,Object> result=hit.getSourceAsMap();
+            System.out.println(result);
+        }
+        System.out.println(resp.getHits().getHits().length);
+
+    }
+
+    @Test
+    public void matchQuery() throws IOException{
+        //1、创建Request
+        SearchRequest request=new SearchRequest(index);
+        request.types(type);
+
+        //2、指定查询条件
+        SearchSourceBuilder builder=new SearchSourceBuilder();
+        builder.query(QueryBuilders.matchQuery("smsContent","收货安装"));
+        request.source(builder);
+
+        //3、执行查询
+        SearchResponse resp=client.search(request,RequestOptions.DEFAULT);
+
+        //4、获取到_source的数据，并展示
+        for(SearchHit hit:resp.getHits().getHits()){
+            Map<String,Object> result=hit.getSourceAsMap();
+            System.out.println(result);
+        }
+
+    }
+
+    @Test
+    public void booleanMatchQuery() throws IOException{
+        //1、创建Request
+        SearchRequest request=new SearchRequest(index);
+        request.types(type);
+
+        //2、指定查询条件
+        SearchSourceBuilder builder=new SearchSourceBuilder();
+        builder.query(QueryBuilders.matchQuery("smsContent","中国 健康")
+                .operator(Operator.OR)
+        );
+        request.source(builder);
+
+        //3、执行查询
+        SearchResponse resp=client.search(request,RequestOptions.DEFAULT);
+
+        //4、获取到_source的数据，并展示
+        for(SearchHit hit:resp.getHits().getHits()){
+            Map<String,Object> result=hit.getSourceAsMap();
+            System.out.println(result);
+        }
+
+
+    }
+
+    @Test
+    public void multiMatchQuery() throws IOException{
+        //1、创建Request
+        SearchRequest request=new SearchRequest(index);
+        request.types(type);
+
+        //2、指定查询条件
+        SearchSourceBuilder builder=new SearchSourceBuilder();
+        builder.query(QueryBuilders.multiMatchQuery("北京","province","smsContent"));
+        request.source(builder);
+
+        //3、执行查询
+        SearchResponse resp=client.search(request,RequestOptions.DEFAULT);
+
+        //4、获取到_source的数据，并展示
+        for(SearchHit hit:resp.getHits().getHits()){
+            Map<String,Object> result=hit.getSourceAsMap();
+            System.out.println(result);
+        }
+    }
+
+
+
+
+
+
 
 
 
