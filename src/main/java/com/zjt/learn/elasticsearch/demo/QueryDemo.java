39418ac5e4197c2a3403b1678032fb39fb654e6c
zhaojiatao <2322795718@qq.com>
2021-06-02 10:55:24 +0800

    聚合查询

--- src/main/java/com/zjt/learn/elasticsearch/demo/QueryDemo.java
+++ src/main/java/com/zjt/learn/elasticsearch/demo/QueryDemo.java
@@ -11,11 +11,18 @@ import org.elasticsearch.index.query.*;
 import org.elasticsearch.index.reindex.BulkByScrollResponse;
 import org.elasticsearch.index.reindex.DeleteByQueryRequest;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.aggregations.Aggregation;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.range.Range;
+import org.elasticsearch.search.aggregations.metrics.cardinality.Cardinality;
+import org.elasticsearch.search.aggregations.metrics.stats.extended.ExtendedStats;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.search.sort.SortOrder;
 import org.junit.Test;
+import sun.reflect.generics.tree.VoidDescriptor;
 
+import javax.lang.model.element.VariableElement;
 import java.io.IOException;
 import java.util.Map;
 
@@ -564,6 +571,81 @@ public class QueryDemo {
             System.out.println(hit.getHighlightFields().get("smsContent"));
         }
 
+    }
+
+
+
+
+    @Test
+    public void cardinality() throws IOException {
+        //1、创建SearchRequest
+        SearchRequest request=new SearchRequest(index);
+        request.types(type);
+
+        //2、指定使用的聚合查询方式
+        SearchSourceBuilder builder=new SearchSourceBuilder();
+        builder.aggregation(AggregationBuilders.cardinality("agg").field("province"));
+        request.source(builder);
+        //3、执行查询
+        SearchResponse resp=client.search(request,RequestOptions.DEFAULT);
+
+        //4、输出结果
+        Cardinality agg = resp.getAggregations().get("agg");
+        long value = agg.getValue();
+        System.out.println(value);
+
+    }
+
+    /**
+     * java实现数值 范围统计
+     * @throws IOException
+     */
+    @Test
+    public void range() throws IOException{
+        SearchRequest request=new SearchRequest(index);
+        request.types(type);
+
+        SearchSourceBuilder builder=new SearchSourceBuilder();
+        builder.aggregation(AggregationBuilders.range("agg").field("fee")
+                .addUnboundedTo(5)
+                .addRange(5,10)
+                .addUnboundedFrom(10)
+        );
+        request.source(builder);
+
+        SearchResponse resp=client.search(request,RequestOptions.DEFAULT);
+
+        Range agg=resp.getAggregations().get("agg");
+        for (Range.Bucket bucket:agg.getBuckets()){
+            String key = bucket.getKeyAsString();
+            Object from = bucket.getFrom();
+            Object to = bucket.getTo();
+            long docCount = bucket.getDocCount();
+            System.out.println(String.format("key:%s,from:%s,to:%s,docCount:%s",key,from,to,docCount));
+        }
+
+
+    }
+
+    /**
+     * 统计聚合查询
+     * @throws IOException
+     */
+    @Test
+    public void extendedStats() throws IOException{
+        SearchRequest request=new SearchRequest(index);
+        request.types(type);
+
+        SearchSourceBuilder builder=new SearchSourceBuilder();
+        builder.aggregation(AggregationBuilders.extendedStats("agg").field("fee"));
+        request.source(builder);
+
+        SearchResponse resp=client.search(request,RequestOptions.DEFAULT);
+
+        ExtendedStats agg=resp.getAggregations().get("agg");
+        double max = agg.getMax();
+        double min = agg.getMin();
+        System.out.println("fee的最大值为："+max+",最小值为："+min);
     }
 
 
